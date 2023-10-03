b832503e63702c86808ffba986752c328032166c
Joseph M. Rutherford <joseph.m.rutherford@ieee.org>
2023-10-03 12:45:13 -0500

    Fixing missing import in quadrature/__init__.py and updating manual

--- doodler/quadrature/__init__.py
+++ doodler/quadrature/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # Copyright (c) 2023, Joseph M. Rutherford
-from .rules import Rule1D, InvalidQuadratureDefinition
+from .rules import Rule1D, Rule2D, InvalidQuadratureDefinition
 from .cached_rules import RuleCache, MissingQuadratureDefinition
