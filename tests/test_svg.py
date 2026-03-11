199661faeda45eff373392dba5c2cf33e4c2d5e0
Joseph M. Rutherford <joseph.m.rutherford@ieee.org>
2026-03-10 21:42:09 -0500

    Adding unit test checks for descriptions read into wires from SVG path
    Copilot assisted

--- tests/test_svg.py
+++ tests/test_svg.py
@@ -6,7 +6,7 @@ import pytest
 import numpy as np
 import pytest
 
-from doodler import read_svg, as_xyz, export_polylines, Real, Unrecoverable, NotYetImplemented, R3Axes, R3Vector, r3vector_equality, real_equality
+from doodler import read_svg, as_xyz, export_polylines, Real, Unrecoverable, NotYetImplemented, WireSegment2D, R3Axes, R3Vector, r3vector_equality, real_equality
 from doodler.r3 import TOLERANCE
 
 
@@ -356,73 +356,97 @@ def test_export_polylines_segment_lengths(tmp_path):
 _SVG_PATH_LINE = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="seg" d="M 0 0 L 10 20"/>
+  <path id="seg" d="M 0 0 L 10 20">
+    <desc>wire segment</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_POLYLINE = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="tri" d="M 0 0 L 5 10 L 10 0"/>
+  <path id="tri" d="M 0 0 L 5 10 L 10 0">
+    <desc>triangle path</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_RELATIVE = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="rel" d="m 1 1 l 2 0 l 0 2"/>
+  <path id="rel" d="m 1 1 l 2 0 l 0 2">
+    <desc>relative path</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_HORIZONTAL_VERTICAL = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="hv" d="M 0 0 H 5 V 3"/>
+  <path id="hv" d="M 0 0 H 5 V 3">
+    <desc>horizontal vertical path</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_RELATIVE_HV = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="rhv" d="M 1 1 h 4 v 2"/>
+  <path id="rhv" d="M 1 1 h 4 v 2">
+    <desc>relative hv path</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_CLOSE = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="sq" d="M 0 0 H 10 V 10 Z"/>
+  <path id="sq" d="M 0 0 H 10 V 10 Z">
+    <desc>closed path</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_IMPLICIT_LINETO = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="imp" d="M 0 0 5 10 10 0"/>
+  <path id="imp" d="M 0 0 5 10 10 0">
+    <desc>implicit lineto</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_ARC = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="arc" d="M 0 0 A 5 5 0 0 1 10 0"/>
+  <path id="arc" d="M 0 0 A 5 5 0 0 1 10 0">
+    <desc>arc path</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_CUBIC = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="cub" d="M 0 0 C 1 1 2 2 3 0"/>
+  <path id="cub" d="M 0 0 C 1 1 2 2 3 0">
+    <desc>cubic bezier</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_QUADRATIC = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="qua" d="M 0 0 Q 5 10 10 0"/>
+  <path id="qua" d="M 0 0 Q 5 10 10 0">
+    <desc>quadratic bezier</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_SMOOTH_CUBIC = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="sc" d="M 0 0 S 5 5 10 0"/>
+  <path id="sc" d="M 0 0 S 5 5 10 0">
+    <desc>smooth cubic bezier</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_SMOOTH_QUADRATIC = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="sq2" d="M 0 0 T 10 0"/>
+  <path id="sq2" d="M 0 0 T 10 0">
+    <desc>smooth quadratic bezier</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_MISSING_ID = '''\
@@ -434,14 +458,20 @@ _SVG_PATH_MISSING_ID = '''\
 _SVG_PATH_EMPTY_D = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="empty" d=""/>
+  <path id="empty" d="">
+    <desc>empty d path</desc>
+  </path>
 </svg>'''
 
 _SVG_PATH_DUPLICATE_ID = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg">
-  <path id="same" d="M 0 0 L 1 1"/>
-  <path id="same" d="M 2 2 L 3 3"/>
+  <path id="same" d="M 0 0 L 1 1">
+    <desc>first path</desc>
+  </path>
+  <path id="same" d="M 2 2 L 3 3">
+    <desc>second path</desc>
+  </path>
 </svg>'''
 
 _SVG_MIXED_ELEMENTS = '''\
@@ -449,14 +479,16 @@ _SVG_MIXED_ELEMENTS = '''\
 <svg xmlns="http://www.w3.org/2000/svg">
   <line id="ln" x1="0" y1="0" x2="1" y2="1"/>
   <polyline id="pl" points="0,0 2,0 2,2"/>
-  <path id="pa" d="M 3 0 L 5 5"/>
+  <path id="pa" d="M 3 0 L 5 5">
+    <desc>mixed path element</desc>
+  </path>
 </svg>'''
 
 
 def test_path_line_two_points(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_LINE))
     assert 'seg' in result
-    pts = result['seg']
+    pts = result['seg'].points
     assert len(pts) == 2
     assert pts[0] == (Real(0), Real(0))
     assert pts[1] == (Real(10), Real(20))
@@ -464,7 +496,7 @@ def test_path_line_two_points(tmp_path):
 
 def test_path_polyline_three_points(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_POLYLINE))
-    pts = result['tri']
+    pts = result['tri'].points
     assert len(pts) == 3
     assert pts[0] == (Real(0), Real(0))
     assert pts[1] == (Real(5), Real(10))
@@ -473,7 +505,7 @@ def test_path_polyline_three_points(tmp_path):
 
 def test_path_relative_commands(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_RELATIVE))
-    pts = result['rel']
+    pts = result['rel'].points
     assert len(pts) == 3
     assert pts[0] == (Real(1), Real(1))
     assert pts[1] == (Real(3), Real(1))
@@ -482,7 +514,7 @@ def test_path_relative_commands(tmp_path):
 
 def test_path_absolute_horizontal_vertical(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_HORIZONTAL_VERTICAL))
-    pts = result['hv']
+    pts = result['hv'].points
     assert len(pts) == 3
     assert pts[0] == (Real(0), Real(0))
     assert pts[1] == (Real(5), Real(0))
@@ -491,7 +523,7 @@ def test_path_absolute_horizontal_vertical(tmp_path):
 
 def test_path_relative_horizontal_vertical(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_RELATIVE_HV))
-    pts = result['rhv']
+    pts = result['rhv'].points
     assert len(pts) == 3
     assert pts[0] == (Real(1), Real(1))
     assert pts[1] == (Real(5), Real(1))
@@ -500,7 +532,7 @@ def test_path_relative_horizontal_vertical(tmp_path):
 
 def test_path_close_appends_start_point(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_CLOSE))
-    pts = result['sq']
+    pts = result['sq'].points
     assert len(pts) == 4
     assert pts[0] == (Real(0), Real(0))
     assert pts[-1] == (Real(0), Real(0))
@@ -508,7 +540,7 @@ def test_path_close_appends_start_point(tmp_path):
 
 def test_path_implicit_lineto_after_move(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_IMPLICIT_LINETO))
-    pts = result['imp']
+    pts = result['imp'].points
     assert len(pts) == 3
     assert pts[0] == (Real(0), Real(0))
     assert pts[1] == (Real(5), Real(10))
@@ -557,8 +589,8 @@ def test_path_duplicate_id_raises(tmp_path):
 
 def test_path_coordinate_dtype(tmp_path):
     result = read_svg(_svg_file(tmp_path, _SVG_PATH_POLYLINE))
-    for pts in result.values():
-        for x, y in pts:
+    for wire_seg in result.values():
+        for x, y in wire_seg.points:
             assert type(x) is Real
             assert type(y) is Real
 
@@ -568,7 +600,98 @@ def test_mixed_line_polyline_path(tmp_path):
     assert set(result.keys()) == {'ln', 'pl', 'pa'}
     assert len(result['ln']) == 2
     assert len(result['pl']) == 3
-    assert len(result['pa']) == 2
+    assert len(result['pa'].points) == 2
+
+
+# ---------------------------------------------------------------------------
+# WireSegment2D and path <desc> tests
+# ---------------------------------------------------------------------------
+
+_SVG_PATH_WITH_DESC = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg">
+  <path id="w" d="M 1 2 L 3 4">
+    <desc>material=copper radius=0.5</desc>
+  </path>
+</svg>'''
+
+_SVG_PATH_MISSING_DESC = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg">
+  <path id="nd" d="M 0 0 L 1 1"/>
+</svg>'''
+
+_SVG_PATH_EMPTY_DESC = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg">
+  <path id="ed" d="M 0 0 L 1 1">
+    <desc></desc>
+  </path>
+</svg>'''
+
+_SVG_PATH_WHITESPACE_DESC = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg">
+  <path id="wd" d="M 0 0 L 1 1">
+    <desc>   </desc>
+  </path>
+</svg>'''
+
+_SVG_PATH_NO_NAMESPACE_DESC = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<svg>
+  <path id="nn" d="M 0 0 L 5 5">
+    <desc>no namespace path</desc>
+  </path>
+</svg>'''
+
+
+def test_path_returns_wire_segment_2d(tmp_path):
+    result = read_svg(_svg_file(tmp_path, _SVG_PATH_WITH_DESC))
+    assert isinstance(result['w'], WireSegment2D)
+
+
+def test_path_description_stored(tmp_path):
+    result = read_svg(_svg_file(tmp_path, _SVG_PATH_WITH_DESC))
+    assert result['w'].description == 'material=copper radius=0.5'
+
+
+def test_path_points_accessible_via_wire_segment(tmp_path):
+    result = read_svg(_svg_file(tmp_path, _SVG_PATH_WITH_DESC))
+    pts = result['w'].points
+    assert pts[0] == (Real(1), Real(2))
+    assert pts[1] == (Real(3), Real(4))
+
+
+def test_path_missing_desc_raises(tmp_path):
+    with pytest.raises(Unrecoverable):
+        read_svg(_svg_file(tmp_path, _SVG_PATH_MISSING_DESC))
+
+
+def test_path_empty_desc_raises(tmp_path):
+    with pytest.raises(Unrecoverable):
+        read_svg(_svg_file(tmp_path, _SVG_PATH_EMPTY_DESC))
+
+
+def test_path_whitespace_desc_raises(tmp_path):
+    with pytest.raises(Unrecoverable):
+        read_svg(_svg_file(tmp_path, _SVG_PATH_WHITESPACE_DESC))
+
+
+def test_path_desc_no_namespace(tmp_path):
+    result = read_svg(_svg_file(tmp_path, _SVG_PATH_NO_NAMESPACE_DESC))
+    assert isinstance(result['nn'], WireSegment2D)
+    assert result['nn'].description == 'no namespace path'
+
+
+def test_wire_segment_2d_has_points_attribute(tmp_path):
+    result = read_svg(_svg_file(tmp_path, _SVG_PATH_WITH_DESC))
+    assert hasattr(result['w'], 'points')
+
+
+def test_wire_segment_2d_has_description_attribute(tmp_path):
+    result = read_svg(_svg_file(tmp_path, _SVG_PATH_WITH_DESC))
+    assert hasattr(result['w'], 'description')
 
 
 
@@ -593,4 +716,3 @@ def test_export_polylines_empty_segments(tmp_path):
 def test_export_polylines_bad_path_raises():
     with pytest.raises(Unrecoverable):
         export_polylines({}, '/nonexistent/directory/out.vtk')
-
