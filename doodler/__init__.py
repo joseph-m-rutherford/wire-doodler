548e89b5472e4b903cb0a4b82bae3db74c671545
Joseph M. Rutherford <joseph.m.rutherford@ieee.org>
2026-03-12 22:52:10 -0500

    Decongesting svg.py into separate files for SVG read and VTK write (Copilot assist)

--- doodler/__init__.py
+++ doodler/__init__.py
@@ -5,5 +5,6 @@ from .common import Index, Integer, Real
 from .errors import Recoverable, Unrecoverable, NeverImplement, NotYetImplemented
 from .geometry import Shape3D, Cylinder, ClippedSphere, LeftHanded, RightHanded, Shape3DSampler, TOLERANCE, WireSegment2D
 from .r3 import R3Axes, R3Vector, Real, r3vector_copy, r3vector_equality, real_equality
-from .svg import read_svg, export_polylines
+from .svg_reader import read_svg
+from .vtk_writer import export_polylines
 from .geometry.wire_segments import as_xyz
